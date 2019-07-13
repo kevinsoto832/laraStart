d92d7c3ef437eecb0e177cc15d6c53990d901042
Kevin Soto <kevinsoto832@gmail.com>
2019-07-13 18:42:41 -0500

    commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <!--
 This is a starter template page. Use this page to start your new project from
@@ -11,12 +10,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
   <title>AdminLTE 3 | Starter</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<div class="wrapper" id="app">
 
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light border-bottom">
@@ -78,15 +78,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
 
             <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                <router-link to="/dashboard" class="nav-link">
+                    <i class="nav-icon fas fa-tachometer-alt blue"></i>
                         <p>Dashboard</p>
-                </a>
+                </router-link>
             </li>
 
             <li class="nav-item has-treeview">
-                <a href="#" class="nav-link active">
-                    <i class="nav-icon fas fa-cog"></i>
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-cog green"></i>
                         <p>Management<i class="right fa fa-angle-left">
                         </i></p>
                 </a>
@@ -94,33 +94,37 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="fas fa-circle nav-icon"></i>
-                  <p>Active Page</p>
+                <router-link to="/users" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Users</p>
                 </a>
               </li>
 
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="fas fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
-                </a>
-              </li>
+          
             </ul>
           </li>
 
           <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="nav-icon fas fa-user"></i>
+                <router-link to="/profile" class="nav-link">
+                  <i class="nav-icon fas fa-user orange"></i>
                     <p>Profile</p>
-                </a>
+                </router-link>
             </li>
             
             <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="nav-icon fas fa-power-off"></i>
-                        <p>logout</p>
-                    </a>
+                    
+
+                    <a class="nav-link" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                                     <i class="nav-icon fas fa-power-off red"></i>
+                                                     <p>{{ __('Logout') }}</p>
+                                        
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
                 </li>
                 
         </ul>
@@ -137,6 +141,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
+        <router-view></router-view>
         
       </div><!-- /.container-fluid -->
     </div>
